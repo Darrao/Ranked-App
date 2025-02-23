a9e3aa53cc2d08e0d72bce6c32f750f85f1f647d
Elio Darras <elio.darras_ext@sonepar.com>
2025-02-23 15:55:37 +0100

    modification env on config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://ranking-app-kodomo-ca4c1991a9c0.herokuapp.com/'
+    ? 'https://ranking-app-kodomo-ca4c1991a9c0.herokuapp.com'
     : 'http://localhost:3000';
 
 export default API_URL;
